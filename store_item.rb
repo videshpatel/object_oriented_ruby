99a582c992af43eeeea77812fb4cf1d910f99d28
“Videsh <videsh.patel@gmail.com>
2019-02-18 21:04:31 -0600

    Hash is finished

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+
+item1 = {:name => "ball", :color => "white", :price => 3.50}
+item2 = {:name => "car", :color => "red", :price => 10.00}
+item3 = {:name => "doll", :color => "brown", :price => 3.50}
+
+p "#{item1[:name]}" "#{item1[:color]}" "#{item1[:price]}"
