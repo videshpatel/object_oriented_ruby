fb6e8d7bb4465897665d00ff16bf1d518d1de3cc
“Videsh <videsh.patel@gmail.com>
2019-02-23 23:42:49 -0600

    added index to my dictionary_app.rb so everthing is listed like Brian showed us.

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -8,14 +8,18 @@ word = gets.chomp
 #make request API
 response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&sourceDictionaries=webster&useCanonical=false&includeTags=false&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
 
-definition = response.parse[0]
+definition = response.parse
 
- word = definition ["word"] 
-#attribText = definition["attributionText"]
-text = definition ["text"]
+#  word = definition ["word"] 
+# #attribText = definition["attributionText"]
+# text = definition ["text"]
 
-p "A #{word} is: #{text}."
-
+# p "A #{word} is: #{text}."
+i = 0
+definition.length.times do 
+  p "#{i + 1}. #{definition[i]["text"]}"
+  i += 1
+end
 
 
 
